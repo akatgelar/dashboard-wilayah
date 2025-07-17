3c25195a9ef273cd600d1c853755249fb5f75478
akatgelar <akatgelar@gmail.com>
2025-07-17 09:25:39 +0700

    feat: engine nextjs tailadmin

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dashboard Wilayah
+
+Tech Stack
+
+- next.js 15
+- tailwind 4
+- react-leaflet 5
+- area by github.com/alifbint/indonesia-38-provinsi
+- geojson by github.com/Alf-Anas/batas-administrasi-indonesia
+- MySQL database
+- Potensi Desa 2018
---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  pageExtensions: ["tsx", "ts"], 
+  output: "standalone",
+};
+
+export default nextConfig;
